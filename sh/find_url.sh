ddc0f8a53b0decd6f69100cbbb39744d0d9225d9
Leo <lion24161582@gmail.com>
2024-05-02 05:08:06 +0000

    修改sh腳本 - 確保其他路徑可以執行

--- sh/find_url.sh
+++ sh/find_url.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-ngrok http http://localhost:9000 --log=stdout > ../log/ngrok.log &
+cd "$(dirname "$0")"
+cd .. 
+ngrok http http://localhost:9000 --log=stdout > log/ngrok.log &
 sleep 3
-cat ../log/ngrok.log
+cat log/ngrok.log
