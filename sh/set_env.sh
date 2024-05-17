4c0cef80b74615edb6527d3bba7eff4cbfca1ba7
cai-yihua <jess92725@gmail.com>
2024-05-17 14:02:35 +0800

    新增 ngrok 安裝腳本

--- sh/set_env.sh
+++ sh/set_env.sh
@@ -10,7 +10,12 @@ cd ..
 source .dev.env 
 
 # 安裝 ngork 
-
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
 # 設定 ngork 
-
+ngrok config add-authtoken $NGROK_AUTHTOKEN
 
